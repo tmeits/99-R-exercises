323f704a4526db1e6e07f1c7bd634c42dac2a867
tmeits <tmeits@gmail.com>
2016-09-30 16:24:22 +0700

    Add ex5 : Reverse a vector

--- R/ex99.R
+++ R/ex99.R
@@ -1,9 +1,12 @@
-# Reproductible research with R
+
+# 99-R-exercises
+#### Reproductible research with R по русски
 
 
-#```{r}
+```{r}
 require(compiler)
-enableJIT()
+enableJIT(level = 3)
+(.packages(all.available=TRUE))
 
 # ex1 : Find the last element of a vector
 find("letters")
@@ -29,6 +32,26 @@ for(i in L) {
 
 cat("number= ", N, "\n") 
 
-#http://homepage.stat.uiowa.edu/~luke/R/compiler/compiler.pdf
-#```
+# ex5 : Reverse a vector
+stopifnot(sort(letters, decreasing = TRUE) == rev(sort(letters)))
+sort(letters, decreasing = TRUE)
+rev(letters)
+letters.rev <- letters
+L <- length(letters)
+for(i in 1: length(letters)) {
+    letters.rev[i] <- letters[L]
+    L <- L -1    
+}
+letters.rev
+
+# http://rus-linux.net/MyLDP/algol/R/r05.html
+
+```
+
+
+
+
+
+
+
 
